d3a43da781588a130694c7216644241794afaa03
Radioactivebun0 <70781995+Radioactivebun0@users.noreply.github.com>
2022-11-03 22:14:56 -0400

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -2,7 +2,7 @@
 
 echo "what was the link that remote.it gave you? "
 read linkk 
-eval "$linkk" >/dev/null
+eval "$linkk"
 echo "Connected to remote.it, starting proxy install..."
 
 echo "Making sure the system is updated..."
